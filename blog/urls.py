6038966b04dd5f18f83772694b17af7e1050fed3
Andriy Yurkiv <andriy.yurkiv@gmail.com>
2023-11-08 16:22:33 +0200

    check black

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.greeting, name="greeting"),
-    path("blog/<int:blog_id>", views.blog_view, name='blog'),
+    path("blog/<int:blog_id>", views.blog_view, name="blog"),
 ]
