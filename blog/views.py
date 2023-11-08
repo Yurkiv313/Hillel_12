62b4ab32e0e09f5a237f4aae059356aeefe88611
Andriy Yurkiv <andriy.yurkiv@gmail.com>
2023-11-08 16:17:02 +0200

    add black

--- blog/views.py
+++ blog/views.py
@@ -6,15 +6,13 @@ from django.views.decorators.cache import cache_page
 from blog.models import Blog
 
 
-# Create your views here.
-
 
 def greeting(request):
-    return render(request, "greeting.html")
+    return render(request, 'greeting.html')
 
 
 @cache_page(60 * 15)
 def blog_view(request, blog_id: int):
     blog = get_object_or_404(Blog, id=blog_id)
     time.sleep(3)
-    return render(request, "blog.html", {"blog": blog})
+    return render(request, "blog.html", {'blog': blog})
